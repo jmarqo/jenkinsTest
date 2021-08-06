0b452c9de8d0745797ce397200c1e7275629a911
EvaTerradillos <eterradillosestevez@gmail.com>
2021-08-06 10:44:49 +0200

    Funcion getGamesByGenre

--- src/services/ServicesGameImpl.java
+++ src/services/ServicesGameImpl.java
@@ -61,8 +61,24 @@ public class ServicesGameImpl implements ServicesGame{
 
 	@Override
 	public List<Game> getGamesByGenre(Genre genre) {
-		// TODO Auto-generated method stub
-		return null;
+		// obtener la lista de juegos
+		
+		List<Game> r = new ArrayList<Game>();	
+		List<Game> games = getGames();
+		
+		// recorrer la lista de juegos
+		
+		
+		for(int ii = 0; ii < games.size(); ii++) {
+			
+			// si el genero coincide con el genero del juego, guardarlo en la nueva lista
+			if(games.get(ii).getGenre().equals(genre)) 
+				r.add(games.get(ii));	
+			
+		}
+		
+		
+		return r;
 	}
 
 	@Override
@@ -73,7 +89,22 @@ public class ServicesGameImpl implements ServicesGame{
 
 	@Override
 	public List<Game> getGamesByPublisher(Publisher publisher) {
-		// TODO Auto-generated method stub
+		
+		// obtener la lista de juegos
+		
+		List<Game> r = new ArrayList<Game>();	
+		List<Game> games = getGames();
+		
+		// recorrer la lista de juegos
+		
+		for(int ii = 0; ii < games.size(); ii++) {
+			
+		}
+				
+		// si el publisher coincide con el publisher del juego, guardarlo en la nueva lista
+				
+
+		
 		return null;
 	}
 
